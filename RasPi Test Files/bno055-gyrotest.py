105cdfd9157d5ec558dcdb3143e0545901eda094
hthurdev <hthurmank2c@outlook.com>
2026-02-10 18:00:48 -0600

    Updating IMU measurement

--- RasPi Test Files/bno055-gyrotest.py
+++ RasPi Test Files/bno055-gyrotest.py
@@ -1,43 +1,32 @@
 import time
-
 import board
-
 import adafruit_bno055
 
 i2c = board.I2C()  
 sensor = adafruit_bno055.BNO055_I2C(i2c)
-last_val = 0xFFFF
 
-
-def temperature():
-    global last_val  # noqa: PLW0603
-    result = sensor.temperature
-    if abs(result - last_val) == 128:
-        result = sensor.temperature
-        if abs(result - last_val) == 128:
-            return 0b00111111 & result
-    last_val = result
-    return result
-
+def get_stabilization_data():
+    try:
+        angles = sensor.euler
+        if angles is None:
+            return 0.0
+        return angles[2]
+    except OSError:
+        return 0.0
 
 while True:
-    
-    #print(
-    #    "Temperature: {} degrees C".format(temperature())
-    #)  # Uncomment if using a Raspberry Pi
-    
+    # Useless info that only slows down the sampling rate:
+    #print("Temperature: {} degrees C".format(temperature()))
     #print(f"Accelerometer (m/s^2): {sensor.acceleration}")
     #print(f"Magnetometer (microteslas): {sensor.magnetic}")
     #print(f"Gyroscope (rad/sec): {sensor.gyro}")
-    
-    #### I only left the Euler Angle uncommented to find position info
-    #### all of the other data works
-    print(f"Euler angle: {sensor.euler}")
-    
-    
-    #print(f"Quaternion: {sensor.quaternion}")
     #print(f"Linear acceleration (m/s^2): {sensor.linear_acceleration}")
     #print(f"Gravity (m/s^2): {sensor.gravity}")
     #print()
+    
+    #Important info for PID loop; Use quaternion if euler becomes erratic.
+    print(f"Euler angle: {sensor.euler}")
+    #print(f"Quaternion: {sensor.quaternion}")
 
+    # Delete this sleep function before testing PID loop.
     time.sleep(1)
