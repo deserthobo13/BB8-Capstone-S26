68b9ed0d4a7fb7717cdb980ef4ccf5bdecb10645
hthurdev <hthurmank2c@outlook.com>
2026-04-18 15:41:52 -0500

    Refactor code structure for improved readability and maintainability

--- BB8 Files/Body Files/Movement_Functions.py
+++ BB8 Files/Body Files/Movement_Functions.py
@@ -21,7 +21,7 @@ class BB8Movement:
         self.imu = IMUSensor()
         
         # Initialize PID with the starter values from your dummy test
-        self.balance_pid = PIDController(kp=1.25, ki=0.0, kd=0.75) 
+        self.balance_pid = PIDController(kp=0.7, ki=0.0, kd=1.2) 
         self.target_pitch = 0.0 # 0 degrees = perfectly upright
         
         # 1. INITIALIZE I2C & PCA9685
